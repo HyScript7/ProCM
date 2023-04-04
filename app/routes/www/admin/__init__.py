71cabfb8f546caff17fe09681a0c16ce295d6f62
HyScript7 <hyscript7@seznam.cz>
2023-04-04 20:38:49 +0200

    Auth Page Split

--- app/routes/www/admin/__init__.py
+++ app/routes/www/admin/__init__.py
@@ -1,6 +1,6 @@
 from common.configuration import HOSTNAME
 from common.route_vars import CSS, JS, NAVBAR
-from flask import render_template
+from flask import render_template, request
 
 from .. import admin
 
@@ -13,6 +13,7 @@ async def root():
 
 @admin.route("/login")
 async def login():
+    login_page = request.args.get("login", False) # Basically, if ?login is at the end of the URL, we render login, otherwise register
     return render_template(
-        "admin/auth.html", hostname=HOSTNAME, css=CSS, js=JS, navbar=NAVBAR
+        "admin/auth.html", hostname=HOSTNAME, css=CSS, js=JS, navbar=NAVBAR, authtype="login" if login_page else "register"
     )
