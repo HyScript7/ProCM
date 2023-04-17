183cfbc1ecc7086194d5c950163b617038690ff7
HyScript7 <hyscript7@seznam.cz>
2023-04-17 12:32:17 +0200

    Background and Paralax Co Authored by Mobilex1122

--- app/common/route_vars.py
+++ app/common/route_vars.py
@@ -2,6 +2,7 @@ CSS = ["/static/css/main.scss.css", "/static/css/icons/font/bootstrap-icons.css"
 JS = [
     "/static/js/popper.min.js",
     "/static/js/bootstrap.min.js",
+    "/static/js/paralax.js",
 ]
 NAVBAR = [
     ["Home", "/", True],
