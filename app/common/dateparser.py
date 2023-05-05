fc5ebbdb6940982eef8c8b3840fd6a457ca8a5c9
HyScript7 <hyscript7@seznam.cz>
2023-05-05 12:27:00 +0200

    Update date parser "Now with more options!"

--- app/common/dateparser.py
+++ app/common/dateparser.py
@@ -1,11 +1,16 @@
 import datetime
 
 
-def parse_date(timestamp: float):
+def parse_date(
+    timestamp: float,
+    show_time: bool = False,
+    show_day_name: bool = True,
+    show_numeric: bool = True,
+    time_sep_symbol: str = "",
+):
     date = datetime.datetime.fromtimestamp(timestamp)
-    # Format the registration date
-    # TODO: Change this to a more effective formatting method,
-    #       because this is probably the single most inefficient
-    #       method to do it.
-    date = f"{['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][date.weekday()]} {date.day}.{date.month}.{date.year}"
-    return date
+    day_name = f"{['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][date.weekday()]} "
+    numeric = f"{date.day}.{date.month}.{date.year}"
+    time = f"{date.hour:0>2}:{date.minute:0>2}{(' ' + time_sep_symbol) if len(time_sep_symbol) else ''} "
+    p_date = f"{time if show_time else ''}{day_name if show_day_name else ''}{numeric if show_numeric else ''}"
+    return p_date
