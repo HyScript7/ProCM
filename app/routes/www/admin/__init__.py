358436985938a320dca1d77cdd8bfce357f14496
HyScript7 <hyscript7@seznam.cz>
2023-05-15 13:12:10 +0200

    Admin Panel Base

--- app/routes/www/admin/__init__.py
+++ app/routes/www/admin/__init__.py
@@ -1,29 +1,49 @@
-from common.blog import latest_posts
 from common.configuration import HOSTNAME
-from common.route_vars import BRAND, CSS, JS, NAVBAR
+from common.route_vars import BRAND, CSS, JS
 from common.sessionparser import get_session
-from common.usercard import User_card
-from flask import render_template, session
+from flask import redirect, render_template, session
 
 from .. import admin
+
+NAVBAR = [
+    ("Dashboard", "bi-columns", "/admin/dashboard"),
+    ("Users", "bi-person", "/admin/users"),
+    ("Blog", "bi-file-earmark-richtext", "/admin/blog"),
+    ("Projects", "bi-journal", "/admin/projects"),
+]
 
 
 @admin.route("/")
 async def root():
     logon = await get_session(session)
-    user_card = None
-    if logon:
-        user_card = await User_card.get(logon[1])
     return render_template(
         "admin/index.html",
         hostname=HOSTNAME,
         css=CSS,
         js=JS,
-        navbar=NAVBAR + ["Admin", "/admin/", True],
-        page="Admin",
+        navbar=NAVBAR,
+        page="Dashboard",
         brand=BRAND,
         title="Admin",
         logon=logon,
-        latest_posts=await latest_posts(),
-        user_card=user_card,
+    )
+
+
+@admin.route("/dashboard/")
+async def dash():
+    return redirect("/admin/")
+
+@admin.route("/<xany>")
+async def test(xany):
+    logon = await get_session(session)
+    return render_template(
+        "admin/index.html",
+        hostname=HOSTNAME,
+        css=CSS,
+        js=JS,
+        navbar=NAVBAR,
+        page=xany.title(),
+        brand=BRAND,
+        title="Admin",
+        logon=logon,
     )
